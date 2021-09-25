612a8343a1e7afc3305c51f89ad7977d191e543c
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-25 12:08:08 +0300

    Updates

---
+++ static/js/main.0cc31090.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwarface=this.webpackJsonpwarface||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),r=n.n(c),s=n(0),a=function(){return Object(s.jsx)("h2",{children:"Hello"})},o=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)(a,{})})};r.a.render(Object(s.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.0cc31090.chunk.js.map
